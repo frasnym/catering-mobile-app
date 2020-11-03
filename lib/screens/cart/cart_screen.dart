6c95df4b6adf963718e8b38a1cf67de591eccf94
frasnym <frastyawan.nym@gmail.com>
2020-11-03 09:34:36 +0800

    Footer Grand Total & Button Checkout

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -13,6 +13,7 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
     final cart = Provider.of<Cart>(context);
     Product product;
     int subTotal;
@@ -32,30 +33,98 @@ class CartScreen extends StatelessWidget {
               .copyWith(fontWeight: FontWeight.bold),
         ),
       ),
-      body: Container(
-        margin: const EdgeInsets.all(kDefaultPaddin),
-        child: ListView.builder(
-          itemCount: cart.items.length,
-          itemBuilder: (context, index) {
-            product = Provider.of<Products>(context, listen: false)
-                .findById(int.parse(cart.items.keys.toList()[index]));
-            subTotal = cart.items.values.toList()[index].quantity *
-                cart.items.values.toList()[index].price;
+      body: Stack(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(
+              top: kDefaultPaddin,
+              right: kDefaultPaddin,
+              left: kDefaultPaddin,
+              bottom: kDefaultPaddin * 3,
+            ),
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (context, index) {
+                product = Provider.of<Products>(context, listen: false)
+                    .findById(int.parse(cart.items.keys.toList()[index]));
+                subTotal = cart.items.values.toList()[index].quantity *
+                    cart.items.values.toList()[index].price;
 
-            return Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ItemImage(product: product),
-                ItemDescription(
-                  cartId: cart.items.keys.toList()[index],
-                  cartItem: cart.items.values.toList()[index],
-                  product: product,
-                  subTotal: subTotal,
+                return Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ItemImage(product: product),
+                    ItemDescription(
+                      cartId: cart.items.keys.toList()[index],
+                      cartItem: cart.items.values.toList()[index],
+                      product: product,
+                      subTotal: subTotal,
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: screenSize.width,
+              padding: const EdgeInsets.symmetric(
+                horizontal: kDefaultPaddin,
+                vertical: kDefaultPaddin / 2,
+              ),
+              decoration: const BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    width: 1,
+                    color: kTextLightColor,
+                  ),
                 ),
-              ],
-            );
-          },
-        ),
+                color: Colors.white,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Grand Total',
+                        style: Theme.of(context).textTheme.headline6.copyWith(
+                              fontSize: 15,
+                              color: kTextLightColor,
+                            ),
+                      ),
+                      Text(
+                        '${formatCurrency.format(cart.totalAmount)}',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      // TODO checkout button
+                    },
+                    child: const Text(
+                      'Checkout',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    color: Colors.red[600],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(kDefaultPaddin / 2),
+                      side: const BorderSide(color: Colors.red),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
